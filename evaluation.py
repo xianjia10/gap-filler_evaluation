3ed67c45f6c503948e0be27f95a88829a7b22793
xianjia10 <xianjia1001@163.com>
2023-11-03 15:00:58 +0800

    first commit

---
+++ evaluation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+import math
+if len(sys.argv) <3:
+    print(f"usege:python {sys.argv[0]} reference_kmers filled_genome_kmers")
+    exit(1)
+
+
+def read_file(path):
+    kmers=set()
+    with open(path) as f:
+        for line in f:
+            line = line.split()
+            kmers.add(line[0])
+    return kmers
+
+
+ref_kmer=read_file(sys.argv[1])
+kmers=read_file(sys.argv[2])
+completeness= len(kmers & ref_kmer) /len(ref_kmer) 
+accuracy =len(kmers & ref_kmer)/len(kmers)
+print("completeness",int(-10*math.log(1-completeness)))
+print("accuracy",int(-10*math.log(1-accuracy)))
+
+
+
